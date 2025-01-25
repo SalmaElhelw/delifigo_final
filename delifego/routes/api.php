2c2d3749d00d5a45c12d8cbf3d4381eb757544e5
Salma <salma.elhelww@gmail.com>
2025-01-25 03:53:23 +0200

    routes

--- delifego/routes/api.php
+++ delifego/routes/api.php
@@ -1,6 +1,13 @@
-use App\Http\Controllers\RestaurantController;
-Route::middleware('api')->group(function () {
+<?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MealController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RestaurantController;
+
+
+Route::middleware('api')->group(function () {
 
 Route::get('restaurants', [RestaurantController::class, 'index']);
 Route::get('restaurants/{id}', [RestaurantController::class, 'show']);
@@ -8,14 +15,10 @@ Route::post('restaurants', [RestaurantController::class, 'store']);
 Route::put('restaurants/{id}', [RestaurantController::class, 'update']);
 Route::delete('restaurants/{id}', [RestaurantController::class, 'destroy']);
 
-
-
 Route::post('meals', [MealController::class, 'store']);
 Route::get('meals/{id}', [MealController::class, 'show']);
 Route::put('meals/{id}', [MealController::class, 'update']);
 Route::delete('meals/{id}', [MealController::class, 'destroy']);
-
-
 
 Route::get('orders', [OrderController::class, 'index']);
 Route::post('orders', [OrderController::class, 'store']);
@@ -23,12 +26,10 @@ Route::get('orders/{id}', [OrderController::class, 'show']);
 Route::put('orders/{id}', [OrderController::class, 'update']);
 Route::delete('orders/{id}', [OrderController::class, 'destroy']);
 
-use App\Http\Controllers\UserController;
-Route::post('users/register', [UserController::class, 'register']);
+Route::post('/register', [UserController::class, 'register']);
 Route::post('users/login', [UserController::class, 'login']);
 Route::get('users/profile', [UserController::class, 'profile'])->middleware('auth:api');
 Route::put('users/update', [UserController::class, 'update'])->middleware('auth:api');
-
 
 Route::middleware('auth:sanctum')->get('/profile', [UserController::class, 'profile']);
 Route::middleware('auth:sanctum')->put('/update', [UserController::class, 'update']);
